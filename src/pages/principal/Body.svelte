dfa138e30d44d398bb80b94f6aca4627e4dc8f0c
Angellomon <angelmtzdiaz@gmail.com>
2021-07-02 15:35:01 -0500

    feat(paginas): Implementación
    + :zap: página de inicio y componentes afines

---
+++ src/pages/principal/Body.svelte
@@ -0,0 +1,21 @@
+<script>
+  import BotonesSeleccion from "../../lib/botones/BotonesMeses.svelte";
+</script>
+
+<section>
+  <h1>Indica a qué edición asististe:</h1>
+  <BotonesSeleccion />
+</section>
+
+<style>
+  section {
+    background-color: #13161c;
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+  }
+  h1 {
+    text-align: center;
+    font-size: 25px;
+  }
+</style>
