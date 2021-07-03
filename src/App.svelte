c9e835104df2ed53c361d92959b7d1a3292880ae
Angellomon <angelmtzdiaz@gmail.com>
2021-07-03 00:04:34 -0500

    feat(fonts): Inclusión
    + :zap: inclusión de tipografías en aplicación

--- src/App.svelte
+++ src/App.svelte
@@ -7,4 +7,12 @@
 </main>
 
 <style>
+  @font-face {
+    font-family: "Futura Condensed Extra Bold";
+    src: url("/fonts/futura-condensed-extra-bold.otf");
+  }
+  @font-face {
+    font-family: "Futura Condensed Medium";
+    src: url("/fonts/futura-condensed-medium.otf");
+  }
 </style>
