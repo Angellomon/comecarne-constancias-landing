f01e744ea4a1d528b9249b7b2a81c172cad34b1d
Angellomon <angelmtzdiaz@gmail.com>
2021-07-03 20:21:42 -0500

    feat(consultas): Páginas
    + :zap: implementación de componentes para consulta de acuerdo a los
    mesesÑ
      + junio
      + julio
      + agosto
      + septiembre

---
+++ src/pages/consultas-mes/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+  import logoSeminario from "../../assets/logo-seminario.png";
+  import cargamento from "../../assets/cargamento.png";
+
+  export let mes = "junio";
+
+  const mesesPermitidos = ["junio", "julio", "agosto", "septiembre"];
+</script>
+
+{#if mesesPermitidos.includes(mes.toLowerCase())}
+  <span>
+    {mes}
+  </span>
+{:else}
+  <span> no </span>
+{/if}
+
+<style>
+  span {
+    color: white;
+  }
+</style>
---
+++ src/pages/consultas-mes/Page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Body from "./Body.svelte";
+  import Header from "./Header.svelte";
+
+  export let params;
+
+  export let currentRoute;
+  console.log(currentRoute, params);
+</script>
+
+<div>
+  <Header mes={currentRoute.namedParams.mes} />
+  <Body />
+</div>
