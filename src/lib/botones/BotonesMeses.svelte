058f967eec654d1c393c2a83d5a690cf74dae27c
Angellomon <angelmtzdiaz@gmail.com>
2021-07-07 15:54:15 -0500

    fix(styles): Botón único

--- src/lib/botones/BotonesMeses.svelte
+++ src/lib/botones/BotonesMeses.svelte
@@ -4,9 +4,9 @@
 
 <div class="botones-seleccion">
   <Boton path="consulta/junio" disabled={false}>JUNIO</Boton>
-  <Boton path="consulta/agosto" disabled={true}>AGOSTO</Boton>
+  <!-- <Boton path="consulta/agosto" disabled={true}>AGOSTO</Boton>
   <Boton path="consulta/julio" disabled={true}>JULIO</Boton>
-  <Boton path="consulta/septiembre" disabled={true}>SEPTIEMBRE</Boton>
+  <Boton path="consulta/septiembre" disabled={true}>SEPTIEMBRE</Boton> -->
 </div>
 
 <style>
