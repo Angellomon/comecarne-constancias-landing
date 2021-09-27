b64a860507552930a758044328ddd2d634fd1fe8
Angellomon <angelmtzdiaz@gmail.com>
2021-09-27 12:07:50 -0500

    feat(evento/septiembre): habilitación de botón

--- src/lib/botones/BotonesMeses.svelte
+++ src/lib/botones/BotonesMeses.svelte
@@ -6,7 +6,7 @@
   <Boton path="consulta/junio" disabled={false}>JUNIO</Boton>
   <Boton path="consulta/julio" disabled={false}>JULIO</Boton>
   <Boton path="consulta/agosto" disabled={false}>AGOSTO</Boton>
-  <!-- <Boton path="consulta/septiembre" disabled={true}>SEPTIEMBRE</Boton> -->
+  <Boton path="consulta/septiembre" disabled={true}>SEPTIEMBRE</Boton>
 </div>
 
 <style>
