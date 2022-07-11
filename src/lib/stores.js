cb6cabef2727aa62017ba312d203565ceeb5c7e1
Angellomon <angelmtzdiaz@gmail.com>
2022-07-11 16:16:55 -0500

    feat(lib/stores): impl stores para logos

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,6 +1,24 @@
-import { writable } from "svelte/store";
+import { readable, writable } from "svelte/store";
 
 export const email = writable("");
 export const isLoading = writable(false);
-export const isError = writable(false)
-export const isInvalid = writable(false)
+export const isError = writable(false);
+export const isInvalid = writable(false);
+
+const logosBase = "/img/logos";
+
+export const logosFooter = readable({
+  sat: `${logosBase}/sat.png`,
+  caaarem: `${logosBase}/caaarem.png`,
+  canadianBeef: `${logosBase}/canadian-beef.png`,
+  usaPoultryEggs: `${logosBase}/usa-poultry-eggs.png`,
+  usaMeat: `${logosBase}/us-meat.png`,
+  senasica: `${logosBase}/senasica.png`,
+  canada: `${logosBase}/canada.png`,
+  usda: `${logosBase}/usda.png`,
+  comecarne: `${logosBase}/comecarne.png`,
+});
+
+export const logosEvento = readable({
+  seminario: `${logosBase}/seminario.png`,
+});
