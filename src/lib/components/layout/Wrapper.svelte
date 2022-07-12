017901bfaa542b724b3796b42164918ca3ed9ff0
Angellomon <angelmtzdiaz@gmail.com>
2022-07-11 19:23:48 -0500

    feat(components): impl wrapper para marco

---
+++ src/lib/components/layout/Wrapper.svelte
@@ -0,0 +1,15 @@
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+    border: 4px solid white;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+</style>
