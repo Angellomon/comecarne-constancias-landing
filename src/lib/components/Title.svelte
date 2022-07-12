d014430eb2da8875253b2208f4b865775071476a
Angellomon <angelmtzdiaz@gmail.com>
2022-07-11 19:23:19 -0500

    feat(components): impl Titulo

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,24 @@
+<div>
+  <h1>CONSTANCIAS</h1>
+</div>
+
+<style>
+  div {
+    background-color: var(--color-secundario);
+    border-top-right-radius: 5vh;
+    border-bottom-left-radius: 5vh;
+
+    padding: 1em 2em;
+    height: 10vh;
+
+    display: flex;
+  }
+
+  h1 {
+    font-family: "Futura Medium Italic";
+    color: white;
+    font-size: 70px;
+
+    align-self: center;
+  }
+</style>
