afdb6e454be5bbc06b462ae223f007b902514011
Angellomon <angelmtzdiaz@gmail.com>
2022-07-12 18:22:15 -0500

    feat: media queries

--- src/lib/components/buttons/Button.svelte
+++ src/lib/components/buttons/Button.svelte
@@ -21,16 +21,28 @@
     background-color: var(--color-principal);
 
     margin: 0.5em 0.5em;
-    padding: 0.3em 1em;
+    padding: 1em 1.5em;
 
     color: white;
     font-size: 1.1em;
-    font-family: "Futura Condensed Extra Bold";
+    font-family: "Montserrat Semi Bold Italic";
 
     cursor: pointer;
 
     border: none;
     border-top-right-radius: 10px;
     border-bottom-left-radius: 10px;
+  }
+
+  @media (min-width: 900px) {
+    button {
+      min-height: 3.5em;
+    }
+  }
+
+  @media (min-width: 1800px) {
+    button {
+      min-height: 6.5em;
+    }
   }
 </style>
