37de9e7b7a59db8d600d74c72ecb5d218a238390
Angellomon <angelmtzdiaz@gmail.com>
2022-08-24 22:54:37 -0500

    chore(styles): styles buttons

--- src/lib/components/buttons/EventsButtons.svelte
+++ src/lib/components/buttons/EventsButtons.svelte
@@ -12,16 +12,20 @@
     <p class="second">NUEVO LAREDO, TAMAULIPAS</p>
   </Boton>
   <Boton path="consulta/2022-julio" disabled={false}>
-    <p class="julio">JULIO</p>
+    <p class="first">12 al 15 de julio</p>
+    <p class="julio">EDICIÓN VIRTUAL</p>
   </Boton>
 </div>
 
 <style>
   div.botones-seleccion {
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
+    align-items: center;
     flex-wrap: wrap;
+    
   }
+
   p {
     color: white;
     font-family: "Montserrat Semi Bold Italic";
@@ -32,44 +36,55 @@
     div.botones-seleccion {
       flex-direction: column;
     }
+
     p.first {
       font-size: 1.2em;
     }
+
     p.second {
       font-size: 0.7em;
     }
+
     p.julio {
-      font-size: 2em;
+      font-size: 1em;
     }
   }
 
   @media (min-width: 900px) and (max-width: 1800px) {
     div.botones-seleccion {
       flex-direction: row;
+      max-width: 70vw;
     }
+
     p.first {
       font-size: 1.9em;
     }
+
     p.second {
       font-size: 1.2em;
     }
+
     p.julio {
-      font-size: 2.3em;
+      font-size: 1.5em;
     }
   }
 
   @media (min-width: 1800px) {
     div.botones-seleccion {
       flex-direction: row;
+      max-width: 50vw;
     }
+
     p.first {
       font-size: 2.2em;
     }
+
     p.second {
       font-size: 1.5em;
     }
+
     p.julio {
-      font-size: 3em;
+      font-size: 1.9em;
     }
   }
 </style>
